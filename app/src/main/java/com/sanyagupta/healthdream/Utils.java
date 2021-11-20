6a5209b17e2ce6ddc210e61cc0d94fe8814cbd51
Sanya_Gupta <sanyag.abc@gmail.com>
2021-11-20 23:50:06 +0530

    Third commit

--- app/src/main/java/com/sanyagupta/healthdream/Utils.java
+++
@@ -1,6 +0,0 @@
-package com.sanyagupta.healthdream;
-
-public class Utils {
-    public static final String EMAIL="ritikagarg2972@gmail.com";
-    public static final String PASSWORD="hyrhzvqntlligufa";
-}
